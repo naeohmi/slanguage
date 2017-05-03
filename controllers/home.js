0d482413e8d37ba423532bab66a73b89446db7ef
naeohmi <naeohmi@gmail.com>
2017-05-03 14:56:03 -0400

    axios fun

--- controllers/home.js
+++ controllers/home.js
@@ -1,22 +1,57 @@
-const urbanDictionary1 = 'http://api.urbandictionary.com/v0/define?';
-const urbanDictionary2 = `term=${word}`;
+    let word = 'hello';
+
+    const urbanDictionaryUrl = 'http://api.urbandictionary.com/v0/define?term=';
+    //const merwebDictionary1 = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/hypocrite?key=';
+    //const merwebDictionary2 = '764f81a5-669d-4172-b6f3-ae92a8403263'; //collegiate level dictionary key
+
+    const oxfordDictionary1 = 'https://od-api.oxforddictionaries.com/api/v1'; //base URL
+    function url() {
+        //config headers with access to Oxford Dictionary API
+        var config = {
+            headers: {
+                "Accept": "application/json",
+                "app_id": "b61ef6b5",
+                "app_key": "eca43cac97b86f34f9f5ae2bb04620fe"
+            }
+        };
+
+        axios.get(`https://od-api.oxforddictionaries.com:443/api/v1/entries/en/${word}/regions=us`, config)
+
+        .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+        //example from axios documentation:
+        //axios.post('/save', { firstName: 'Marlon' }, config);
 
 
-const merwebDictionary1 = 'http://www.dictionaryapi.com/api/v1/references/collegiate/xml/hypocrite?key=';
-const merwebDictionary2 = '764f81a5-669d-4172-b6f3-ae92a8403263'; //collegiate level dictionary key
+        //save whole object as a variable
+        //go through the whole object and grab just the definition as one big query
+        var dictionaryObj = {
+            //results.lexicalEntries.entries.senses.definitions
+        };
+    };
 
 
-const oxfordDictionary1 = ' https://od-api.oxforddictionaries.com/api/v1'; //base URL
+    let click = () => {
+        const submit = document.getElementById('submit');
+        submit.addEventListener('click', () => {
+            let input = document.getElementById('zip').value;
+            displayDefs(input)
+        });
+    };
 
-//config headers with access to Oxford Dictionary API
-var config = {
-    headers: {
-        "Accept": "application/json",
-        "app_id": "b61ef6b5",
-        "app_key": "eca43cac97b86f34f9f5ae2bb04620fe"
+    let displayDefs = (word) => {
+        axios(urbanDictionaryUrl + word)
+            .then((d) => {
+                return d.data;
+                console.log(d.data);
+            })
+    };
+
+    module.exports = {
+        url: url,
+        click: click
     }
-};
-
-axios.get(`https://od-api.oxforddictionaries.com:443/api/v1/entries/en/${word}/regions=us`, config);
-//example from axios documentation:
-//axios.post('/save', { firstName: 'Marlon' }, config);
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+var home = require('./home.js');
 
 router.get('/', (req, res, next) => {
+    console.log(home.url());
     res.render('index', {
         title: 'SLANGUAGE'
     });
